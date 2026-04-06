26e038850ff1ce3b42e176de4b05df1b7af024de
miguelpiedade2007 <miguel.piedade2007@gmail.com>
2026-04-06 10:41:44 -0300

    mais funcaos.py adicionado

---
+++ aula03_modulo_condicionais/mais_funcaos.py
@@ -0,0 +1,11 @@
+# Função sem retorno e com
+def boas_vindas(nome):
+    print(f"Olá, {nome}! Seja bem-vindo!")
+nome_usuario = "Miguel"
+boas_vindas(nome_usuario)
+
+# Função com retorno e com Param.
+def soma(num_a, num_b):
+    soma = num_a + num_b
+    return soma
+soma( 2, 8)

b62849d0d861ed9df2acff14fd9688cfa34a137d
Miguel Piedade <miguel.piedade2007@gmail.com>
2026-04-11 18:44:59 -0300

    exerc_9_completo

--- Aula4/exerc_9.py
+++ Aula4/exerc_9.py
@@ -1,10 +1,16 @@
 print("Todos os números primos de 2 a 2000")
-n = 0
 
-if n
+for x in range(2, 2001):
+    primo = True
+    for i in range(2, x//2 + 1):
+        if x % i ==0:
+            primo = False
+            break
+
+
+    if primo:
+        print(x)
 
 
 
-for x in range (2, 2001):
-    n += x
-    print(n)
+
